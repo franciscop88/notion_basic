c3e0030cb67fbf580381f17480c8c349230f6b5f
Francisco Piccirilli <francisco.piccirilli@gmail.com>
2024-02-11 18:15:58 +0100

    primer commit

---
+++ operadores_3.js
@@ -0,0 +1,29 @@
+//1.1 Multiplicar 10 por 5 y mostrar el resultado
+
+var resultadoMultiplicacion = 10 * 5;
+console.log(resultadoMultiplicacion); // Debería mostrar: 50
+
+//1.2 Dividir 10 por 2 y mostrar el resultado
+
+var resultadoDivision = 10 / 2
+console.log (resultadoDivision); //Deberia mostrar 5
+
+//1.3 Mostrar el resto de dividir 15 por 9
+
+var restoDivision = 15 % 9;
+console.log(restoDivision); // Debería mostrar: 6
+
+// 1.4 Usar el operador de asignación para o = 15, con p = 10 y j = 5
+
+var p = 10;
+var j = 5;
+var o = p + j;
+console.log(o); // Debería mostrar: 15
+
+// 1.5 Usar el operador de asignación para i = 50, con c = 10 y m = 5
+var c = 10;
+var m = 5;
+var i = c * m;
+console.log(i); // Debería mostrar: 50
+
+
---
+++ variables_1.js
@@ -0,0 +1,18 @@
+// 1.1
+var myFavoriteHero = 'Hulk';
+
+// 1.2
+var x = 50;
+
+// 1.3
+var h = 5;
+var y = 10;
+
+// 1.4
+var z = h + y;
+
+// Puedes imprimir los resultados en la consola para verificar
+console.log('1.1 myFavoriteHero:', myFavoriteHero);
+console.log('1.2 x:', x);
+console.log('1.3 h:', h, 'y:', y);
+console.log('1.4 z:', z);
