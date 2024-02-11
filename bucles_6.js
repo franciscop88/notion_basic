dc949760e15bc8122056e777e485f04a934e1712
Francisco Piccirilli <francisco.piccirilli@gmail.com>
2024-02-11 20:57:29 +0100

    commits de bucles

---
+++ bucles_6.js
@@ -0,0 +1,22 @@
+
+  // 1.1 Bucle for que va desde 0 a 9 y lo muestra por consola:
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+  //1.2 Bucle for que va desde 0 a 9 y lo muestrar por consola solo cuando el resto del número dividido entre 2 sea 0:
+
+  for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+  
+  //1.3 Bucle para contar ovejas que da 10 vueltas:
+  for (let i = 1; i <= 10; i++) {
+    if (i === 10) {
+      console.log('Dormido!');
+    } else {
+      console.log('Intentando dormir 🐑');
+    }
+  }
+  
